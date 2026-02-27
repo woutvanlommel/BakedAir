42c3295aa784590356fb3b60eada5fc14e524f26
woutvanlommel <woutvanlommel@icloud.com>
2026-02-27 13:05:59 +0100

    json data voor de navigatie

--- includes/header.php
+++ includes/header.php
@@ -11,14 +11,32 @@
 
 <body>
 
+    <?php
+    // Laad de navigatie data in
+    $navJson = file_get_contents('data/navigation.json');
+    $navItems = json_decode($navJson, true);
+    ?>
+
     <nav class="navbar">
         <div class="container nav-flex">
-            <div class="nav-brand"><a href="#hero">Oxy<span>Pure</span></a></div>
+            <a href="index.php" class="nav-brand">Oxy<span>Pure</span></a>
+
+            <input type="checkbox" id="menu-toggle">
+            <label for="menu-toggle" class="menu-icon">&#9776;</label>
+
             <ul class="nav-links">
-                <li><a href="#waarom">Onze Missie</a></li>
-                <li><a href="#collectie">De Shop</a></li>
-                <li><a href="#ervaringen">Resultaten</a></li>
-                <li><a href="https://ko-fi.com/jouwlink" class="nav-cta">Claim Uw Briesje</a></li>
+                <?php foreach ($navItems as $item): ?>
+                    <li>
+                        <a href="<?php echo $item['url']; ?>"
+                            class="<?php echo $item['is_cta'] ? 'nav-cta' : ''; ?>"
+                            <?php if ($item['is_cta']): ?>
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            <?php endif; ?>>
+                            <?php echo $item['name']; ?>
+                        </a>
+                    </li>
+                <?php endforeach; ?>
             </ul>
         </div>
     </nav>
